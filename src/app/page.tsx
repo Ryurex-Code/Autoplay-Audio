0cbfe3bd1606aa3b59ecd8a6aa564b1a62983d6d
Ryurex-Code <m.rafiking17@gmail.com>
2025-11-21 10:11:21 +0800

    Bug Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   const wakeLockRef = useRef<any>(null);
 
   // Default play time (bisa diatur sesuai kebutuhan)
-  const defaultPlayTime = "09:55";
+  const defaultPlayTime = "10:00";
 
   // Keep screen awake - Request Wake Lock
   useEffect(() => {
